e8248d372a525e4897b79e87e18d3ce9fd326c47
Aabritikoirala <aabritikoirala10@gmail.com>
2024-01-26 12:40:32 +0545

    Your commit message

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,7 +4,7 @@ export default function AppHeader() {
     return (
         <nav className="bg-blue-600 h-12 py-2 px-3 flex justify-between items-center">
             <div className='brand text-xl font-semibold'>
-                <h1>Aabru</h1>
+                <h1>Aabriti</h1>
             </div>
             <div>
                 <ul className='flex space-x-6 font-bold'>
