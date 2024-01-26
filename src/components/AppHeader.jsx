8f303bc356a61644e25c77099d2291538a98e46f
AabritiKoirala <aabritikoirala10@gmail.com>
2024-01-26 12:05:02 +0545

    first

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function AppHeader() {
+    return (
+        <nav className="bg-blue-600 h-12 py-2 px-3 flex justify-between items-center">
+            <div className='brand text-xl font-semibold'>
+                <h1>Aabru</h1>
+            </div>
+            <div>
+                <ul className='flex space-x-6 font-bold'>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">About Us</a></li>
+                    
+                </ul>
+                    
+    
+            </div>
+           </nav>
+  )
+}
